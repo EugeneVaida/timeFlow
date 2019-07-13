4f24e68f6dd8e15a4bce0bef66f3c760b8a0cc1f
e.v.e.r.e.s.tt.1551 <e.v.e.r.e.s.tt.1551@gmail.com>
2019-07-14 01:47:07 +0300

    First functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  result : string = '';
+  dates = { start: '', end: '' };
   constructor() {}
 
+  calculate(){
+    console.log(this.dates);
+    let end = new Date(this.dates.end); // some mock date
+    let millisecondsEnd : number= end.getTime(); 
+    let start = new Date(this.dates.start); // some mock date
+    let millisecondsStart : number = start.getTime();
+    let hours : string = ((millisecondsEnd - millisecondsStart) / 1000 / 60 / 60).toFixed(2);
+    this.result = `Your time is ${hours}`;
+  }
 }
